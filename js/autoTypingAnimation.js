5c26f9c24007d7a00c77c0bbe0f8bf1690d6c578
bilalansar3 <bilalahmadai971@gmail.com>
2023-03-25 14:15:09 +0500

    1st

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "I am always passionate about helping clients achieve their business goals and drive success online. I provide guidance and support in all aspects of WordPress development and am committed to providing excellent customer service and ongoing support to my clients."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
